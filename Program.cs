48f69bc21737ac9672cf24f8d8d66ad1c8590615
Bruno Barbosa <brunobarbosa90@hotmail.com>
2024-08-19 10:18:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pais = "Brasil";
+            string continente = "América do Sul";
+
+            Console.WriteLine(pais);
+            Console.WriteLine(continente);
+
+            pais = "Escócia";
+            continente = "Europa";
+
+            Console.WriteLine(pais);
+            Console.WriteLine(continente);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
